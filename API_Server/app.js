c457c27dc319846833adcd6d7b09427529a3dce4
makehoney <makehoney3@gmail.com>
2018-10-10 23:45:41 +0900

    add cors module

--- API_Server/app.js
+++ API_Server/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+let cors = require('cors')
 let testRouter = require('./api/test/routes')
 
 var app = express();
@@ -12,6 +12,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
