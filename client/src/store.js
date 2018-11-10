d6bc77bbec3ab1123d39b31965f367a3020d9930
makehoney <makehoney3@gmail.com>
2018-11-10 23:22:37 +0900

    change utils structures

--- client/src/store.js
+++ client/src/store.js
@@ -1,73 +1,81 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import getWeb3 from './utils/getWeb3';
-import getContract from './utils/getContract';
+import { getWeb3, pollWeb3 } from './utils/getWeb3';
+import { getContract } from './utils/getContract';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    web3: {
-        isInjected: false,
-        web3Instance: null,
-        networkID: null,
-        coinbase: null,
-        balance: null,
-        error: null
-    },
-    contractInstance: null,
-    artists: {
-        addresses: null,
-        names: null
-    }
-  },
-  mutations: {
-    registerWeb3Instance (state, payload) {
-        console.log('registerWeb3Instance Mutation being executed', payload);
-
-        /* web3Copy 없이 리팩토링해보기 */
-        let result = payload;
-        let web3Copy = state.web3;
-        web3Copy.coinbase = result.coinbase;
-        web3Copy.networkID = result.networkID;
-        web3Copy.balance = parseInt(result.balance, 10);
-        web3Copy.isInjected = result.isInjected;
-        web3Copy.web3Instance = result.web3;
-        state.web3 = web3Copy;
-    },
-    registerContractInstance (state, payload) {
-        state.contractInstance = () => payload;
-    },
-    setArtistAddresses(state, payload) {
-        state.artists.addresses = payload
-    },
-    setArtistNames(state, payload) {
-        state.artists.names = payload
-    }
-  },
-  actions: {
-    async registerWeb3({ commit }) {
-        console.log('registerWeb3 Action being executed');
-
-        let result = await getWeb3
-        .catch(err => { console.error('error in action registerWeb3', err) });
-        commit('registerWeb3Instance', result);
-    },
-    async getContractInstance({ commit }) {
-        let result = await getContract
-        .catch(err => { console.error('error in action getContractInstance', err) });
-        commit('registerContractInstance', result);
-    },
-    async getArtistAddresses({ commit, state }) {
-        let result = await state.contractInstance().methods.getAllArtistsAddrs().call()
-        commit('setArtistAddresses', result)
-    },
-    async getArtistNames({ commit, state }) {
-        let result = []
-        for(let i = 0; i < state.artists.addresses.length; i++) {
-            result.push(await state.contractInstance().methods.getArtistNameByIndex(i).call())
+    state: {
+        web3: {
+            web3Instance: null,
+            networkID: null,
+            coinbase: null,
+            balance: null
+        },
+        contractInstance: null,
+        artists: {
+            addresses: null,
+            names: null
         }
-        commit('setArtistNames', result)
+    },
+    mutations: {
+        setWeb3Meta (state, payload) {
+            let web3Copy = state.web3
+            web3Copy.web3Instance = payload.web3
+            web3Copy.networkID = payload.networkID
+            web3Copy.coinbase = payload.coinbase
+            web3Copy.balance = parseInt(payload.balance, 10)
+            state.web3 = web3Copy
+            pollWeb3(state)
+        },
+        changeCoinbase (state, payload) {
+            state.web3.coinbase = payload.coinbase
+            state.web3.balance = parseInt(payload.balance, 10)
+        },
+        setContractInstance (state, payload) {
+            state.contractInstance = () => payload
+        },
+        resetWeb3Instance (state) {
+            state.web3.web3Instance = null
+            state.web3.networkID = null
+            state.web3.coinbase = null
+            state.web3.balance = null
+        },
+        setArtistAddresses(state, payload) {
+            state.artists.addresses = payload
+        },
+        setArtistNames(state, payload) {
+            state.artists.names = payload
+        }
+    },
+    actions: {
+        async checkWeb3({ commit, state }) {
+            try {
+                let result = await getWeb3
+                commit('setWeb3Meta', result)
+            } catch (err) {
+                pollWeb3(state)
+            }
+        },
+        async getContractInstance({ commit }) {
+            try {
+                let result = await getContract
+                commit('setContractInstance', result)
+            } catch (err) {
+                throw console.error('Error in action getContractInstance', err)
+            }
+        },
+        async getArtistAddresses({ commit, state }) {
+            let result = await state.contractInstance().methods.getAllArtistsAddrs().call()
+            commit('setArtistAddresses', result)
+        },
+        async getArtistNames({ commit, state }) {
+            let result = []
+            for(let i = 0; i < state.artists.addresses.length; i++) {
+                result.push(await state.contractInstance().methods.getArtistNameByIndex(i).call())
+            }
+            commit('setArtistNames', result)
+        }
     }
-  }
 })
---
+++ client/src/utils/getWeb3/index.js
@@ -0,0 +1,76 @@
+import Web3 from 'web3'
+import store from '../../store'
+
+export const getWeb3 = new Promise((resolve, reject) => {
+    let isUserConnected;
+    const web3 = new Web3(window.web3.currentProvider)
+    const coinbase = web3.eth.getAccounts()[0]
+    typeof coinbase === 'undefined'
+        ? isUserConnected = false
+        : isUserConnected = true
+
+    if (isUserConnected) {
+        resolve({
+            web3() {
+                return web3
+            },
+            coinbase
+        })
+    } else {
+        reject(new Error('Unable to connect to Metamask'))
+    }
+}).then(result => {
+    return new Promise((resolve, reject) => {
+        result.web3().eth.net.getNetworkType((err, networkID) => {
+            if(err) {
+                reject(new Error('Unable to retrieve network ID'))
+            } else {
+                result = Object.assign({}, result, { networkID })
+                resolve(result)
+            }
+        })
+    })
+}).then(result => {
+    return new Promise((resolve, reject) => {
+        result.web3().eth.getBalance(result.coinbase, (err, balance) => {
+            if (err) {
+                reject(new Error(`Unable to retrieve balance for address: ${result.coinbase}`))
+            } else {
+                result = Object.assign({}, result, { balance })
+                resolve(result)
+            }
+        })
+    })
+})
+
+export const pollWeb3 = state => {
+    let web3 = new Web3(window.web3.currentProvider)
+
+    setInterval(async () => {
+        if (typeof (await web3.eth.getAccounts())[0] === 'undefined') {
+            store.commit('resetWeb3Instance')
+        } else if (state.web3.web3Instance) {
+            if ((await web3.eth.getAccounts())[0] !== store.state.web3.coinbase) {
+                let newCoinbase = (await web3.eth.getAccounts())[0]
+                web3.eth.getBalance(newCoinbase, (err, newBalance) => {
+                    if (err) {
+                        // 수정 필요
+                        console.log(err)
+                    } else {
+                        store.commit('pollWeb3Instance', {
+                            coinbase: newCoinbase,
+                            balance: parseInt(newBalance, 10)
+                        })
+                    }
+                })
+            }
+        } else {
+            let web3Copy = state.web3
+            web3Copy.web3Instance = () => web3
+            web3Copy.networkID = await web3.eth.net.getNetworkType()
+            web3Copy.coinbase = (await web3.eth.getAccounts())[0]
+            web3Copy.balance =  await web3.eth.getBalance(state.web3.coinbase)
+            state.web3 = web3Copy
+        }
+    }, 2000)
+}
