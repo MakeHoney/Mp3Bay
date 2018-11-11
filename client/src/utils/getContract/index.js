659e9e4f0b1b06b975a618808a3a9cebfae93f15
makehoney <makehoney3@gmail.com>
2018-11-11 16:46:17 +0900

    Apply gitignore

--- client/src/utils/getContract/index.js
+++ client/src/utils/getContract/index.js
@@ -1,5 +1,5 @@
 import Web3 from 'web3'
-import { ABI, address } from './managerContract'
+import { ABI, address } from './contracts/manager'
 
 export const getContract = new Promise((resolve, reject) => {
     let web3 = new Web3(window.web3.currentProvider)
