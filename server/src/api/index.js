dbabc27e87434b950e24fe6e0ff6c634e4c00a27
makehoney <makehoney3@gmail.com>
2018-11-27 07:38:56 +0900

    add music router

---
+++ server/src/api/index.js
@@ -0,0 +1,6 @@
+import express from 'express'
+const router = express.Router()
+
+router.use('/music')
+
+export default router
---
+++ server/src/api/music/index.js
@@ -0,0 +1,6 @@
+import express from 'express'
+const router = express.Router()
+
+router.get('/:id')
+
+export default router
