4b9d1b0ade83d87d2e73244f3590f0bafa49cf60
makehoney <makehoney3@gmail.com>
2018-11-27 07:46:29 +0900

    add app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,17 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import api from './api'
+
+const app = express()
+
+app.set('port', process.env.PORT || 8888)
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ urlencoded: true }))
+app.use('/', api)
+
+app.listen(app.get('port'), () => {
+  console.log(`Server running on port ${app.get('port')}`)
+})
