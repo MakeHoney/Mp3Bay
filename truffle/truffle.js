c627f48635b2275a1ef824aa4c2a7b66c11eecd4
makehoney <makehoney3@gmail.com>
2018-12-01 16:12:58 +0900

    edit gitignore

--- truffle/truffle.js
+++ truffle/truffle.js
@@ -2,7 +2,7 @@ module.exports = {
   networks: {
     development: {
       host: 'localhost',
-      port: 8822,
+      port: 8545,
       network_id: '*', // Match any network id
       gas: 4712388
     }
