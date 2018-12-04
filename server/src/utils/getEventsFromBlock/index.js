69f8826041cb576a2c9fc369a2cc899165a6f707
makehoney <makehoney3@gmail.com>
2018-12-05 01:12:07 +0900

    getEventFromBlock util needs to be refactored

--- server/src/utils/getEventsFromBlock/index.js
+++ server/src/utils/getEventsFromBlock/index.js
@@ -2,7 +2,6 @@ import { utils } from '../index'
 
 export const getEventsFromBlock = async (eventName, filter) => {
   // filter : const filter = {songID: data(array)}
-  let songArray = []
   const option = {
     fromBlock: 0,
     toBlock: 'latest'
@@ -16,16 +15,30 @@ export const getEventsFromBlock = async (eventName, filter) => {
 
   if(!events.length) throw new Error("Song doesn't exist")
 
+  // TODO: 이하 코드 밖으로 빼기 (이벤트 종류에따라 달라짐)
+
   // song event에 artist 메타데이터 추가
+  // let songArray = []
+  // for (let i in events) {
+  //   const songData = events[i].returnValues
+  //   const obj = Object.assign(
+  //     { songID: songData.songID },
+  //     { ipfsHash: songData.ipfsHash },
+  //     { title: songData.title  })
+  //   songArray.push(obj)
+  //   // songArray[i].data = await utils.lib.ipfsService.loadObjFromFile(songArray[i].ipfsHash)
+  // }
+  // console.log(songArray)
+  // return songArray
+
+  let picArray = []
   for (let i in events) {
-    const songData = events[i].returnValues
+    const picData = events[i].returnValues
     const obj = Object.assign(
-      { songID: songData.songID },
-      { ipfsHash: songData.ipfsHash },
-      { title: songData.title  })
-    songArray.push(obj)
-    // songArray[i].data = await utils.lib.ipfsService.loadObjFromFile(songArray[i].ipfsHash)
+      { artistID: picData.artistID },
+      { pictureHash: picData.pictureHash }
+    )
+    picArray.push(obj)
   }
-  console.log(songArray)
-  return songArray
+  return picArray
 }
