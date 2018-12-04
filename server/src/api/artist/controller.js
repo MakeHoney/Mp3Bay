9a2443c3068fd6f1617049fe89f929e4ddef90e8
makehoney <makehoney3@gmail.com>
2018-12-05 00:05:04 +0900

    complete basic artist controller

--- server/src/api/artist/controller.js
+++ server/src/api/artist/controller.js
@@ -3,7 +3,7 @@ import { utils } from '../../utils'
 export const controller = {
   async savePicture (req, res) {
     try {
-      const file = req.picture.buffer
+      const file = req.file.buffer
       const ipfsHash = await utils.lib.ipfsService.saveObjAsFile({ picture: file })
       res.json({
         ipfsHash
@@ -16,7 +16,17 @@ export const controller = {
   },
   async loadPicture (req, res) {
     try {
-      // artistID 인자로 받기
+      const filter = { artistID }
+      // // 일단은 음원 한개만
+      const songs = await utils.getEventsFromBlock('ArtistCreated', filter)
+      const ipfsHash = songs[0].ipfsHash
+
+      const { picture } = await utils.lib.ipfsService.loadObjFromFile('QmPx2caFcCtf4R5QDsVhgn7y8Hni7KuxVMQArTwG7BuVdE')
+      console.log(picture)
+      const file = Buffer.from(picture.data)
+
+      res.writeHead(200, {'Content-Type': 'image/gif' })
+      res.end(file, 'binary')
     } catch (err) {
       res.status(500).json({
         message: err.message
--- server/src/api/artist/index.js
+++ server/src/api/artist/index.js
@@ -1,7 +1,10 @@
 import { Router } from 'express'
 import { controller } from './controller'
+import multer from 'multer'
+const upload = multer({ storage: multer.memoryStorage() })
 const router = Router()
 
-router.post('/upload-pic', controller.savePicture)
+router.get('/load-picture', controller.loadPicture)
+router.post('/upload-picture', upload.single('picture'), controller.savePicture)
 
 export default router
