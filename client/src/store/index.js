2c48974b5f6b5e41b7b3f9eef44ff3b38dab2c7d
makehoney <makehoney3@gmail.com>
2018-12-05 00:45:47 +0900

    4 -> 2 indent

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,42 +5,42 @@ import { blockSync } from './modules'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    modules: {
-        blockSync
+  modules: {
+    blockSync
+  },
+  state: {
+    user: {
+      type: null,
+      name: null,
+      address: null
     },
-    state: {
-        user: {
-            type: null,
-            name: null,
-            address: null
-        },
-        artists: {
-            addresses: null,
-            names: null
-        },
-        listeners: {
+    artists: {
+      addresses: null,
+      names: null
+    },
+    listeners: {
 
-        }
-    },
-    mutations: {
-        setArtistAddresses(state, payload) {
-            state.artists.addresses = payload
-        },
-        setArtistNames(state, payload) {
-            state.artists.names = payload
-        }
-    },
-    actions: {
-        async getArtistAddresses({ commit, state }) {
-            let result = await state.blockSync.contractInstance().methods.getAllArtistsAddrs().call()
-            commit('setArtistAddresses', result)
-        },
-        async getArtistNames({ commit, state }) {
-            let result = []
-            for(let i = 0; i < state.artists.addresses.length; i++) {
-                result.push(await state.blockSync.contractInstance().methods.getArtistNameByIndex(i).call())
-            }
-            commit('setArtistNames', result)
-        }
     }
+  },
+  mutations: {
+    setArtistAddresses(state, payload) {
+      state.artists.addresses = payload
+    },
+    setArtistNames(state, payload) {
+      state.artists.names = payload
+    }
+  },
+  actions: {
+    async getArtistAddresses({ commit, state }) {
+      let result = await state.blockSync.contractInstance().methods.getAllArtistsAddrs().call()
+      commit('setArtistAddresses', result)
+    },
+    async getArtistNames({ commit, state }) {
+      let result = []
+      for(let i = 0; i < state.artists.addresses.length; i++) {
+        result.push(await state.blockSync.contractInstance().methods.getArtistNameByIndex(i).call())
+      }
+      commit('setArtistNames', result)
+    }
+  }
 })
