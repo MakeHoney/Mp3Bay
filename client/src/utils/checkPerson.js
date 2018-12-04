d8d28e8f4c60422ac66c25a332fd8791626699b2
makehoney <makehoney3@gmail.com>
2018-12-04 19:40:30 +0900

    4 indent -> 2 indent

--- client/src/utils/checkPerson.js
+++ client/src/utils/checkPerson.js
@@ -5,63 +5,64 @@ import store from '@/store'
  */
 
 export const CheckPerson = {
-    async isArtist() {
-        let flag
-        let contractMethods = store.getters['blockSync/contractMethods']
-        let userAccount = store.state.blockSync.web3.coinbase
+  async isArtist() {
+    let flag
+    let contractMethods = store.getters['blockSync/contractMethods']
+    let userAccount = store.state.blockSync.web3.coinbase
 
-        try {
-          let artistAddr = await contractMethods.accountToArtistAddr(userAccount).call()
-            artistAddr === '0x0000000000000000000000000000000000000000' || artistAddr === null
-            ? flag = false
-            : flag = true
-            return flag
-        } catch (err) {
-            console.error('error in checkPerson', err)
-        }
-    },
-    async isListener() {
-        let flag
-        let contractMethods = store.getters['blockSync/contractMethods']
-        let userAccount = store.state.blockSync.web3.coinbase
-
-        try {
-            let listenerAddr = await contractMethods.accountToListenerAddr(userAccount).call()
-            listenerAddr === '0x0000000000000000000000000000000000000000' || listenerAddr === null
-            ? flag = false
-            : flag = true
-            return flag
-        } catch (err) {
-            console.error('error in checkPerson', err)
-        }
-    },
-    async userType () {
-        if (!(await this.isArtist()) && !(await this.isListener())) {
-            store.state.user.name = null
-            return null
-        } else if (await this.isArtist()) {
-            let contractMethods = store.getters['blockSync/contractMethods']
-            let userAccount = store.state.blockSync.web3.coinbase
-            // solidity에서 return value 2개 이상일시 destructuring assignment로 받는 것인지 확인하기
-            let { name } = await contractMethods.getArtistByAcc(userAccount).call()
-            // store.state.user.address는 어디서 초기화 할지 생각 // 해당 프로퍼티가 필요한지도 생각
-            store.state.user.name = name
-            return 'Artist'
-        } else {
-            let contractMethods = store.getters['blockSync/contractMethods']
-            let userAccount = store.state.blockSync.web3.coinbase
-          console.log(userAccount)
-
-
-          try {
-            let name = await contractMethods.getListenerByAcc(userAccount).call()
-          } catch (err) {
-                console.log(err)
-          }
-
-          console.log('flag2')
-            store.state.user.name = name
-            return 'Listener'
-        }
+    try {
+      let artistAddr = await contractMethods.accountToArtistAddr(userAccount).call()
+      artistAddr === '0x0000000000000000000000000000000000000000' || artistAddr === null
+        ? flag = false
+        : flag = true
+      return flag
+    } catch (err) {
+      console.error('error in checkPerson', err)
     }
+  },
+  async isListener() {
+    let flag
+    let contractMethods = store.getters['blockSync/contractMethods']
+    let userAccount = store.state.blockSync.web3.coinbase
+
+    try {
+      let listenerAddr = await contractMethods.accountToListenerAddr(userAccount).call()
+      listenerAddr === '0x0000000000000000000000000000000000000000' || listenerAddr === null
+        ? flag = false
+        : flag = true
+      return flag
+    } catch (err) {
+      console.error('error in checkPerson', err)
+    }
+  },
+  async userType () {
+    if (!(await this.isArtist()) && !(await this.isListener())) {
+      store.state.user.name = null
+      return null
+    } else if (await this.isArtist()) {
+      let contractMethods = store.getters['blockSync/contractMethods']
+      let userAccount = store.state.blockSync.web3.coinbase
+      // solidity에서 return value 2개 이상일시 destructuring assignment로 받는 것인지 확인하기
+      let { name } = await contractMethods.getArtistByAcc(userAccount).call()
+      // store.state.user.address는 어디서 초기화 할지 생각 // 해당 프로퍼티가 필요한지도 생각
+      store.state.user.name = name
+      return 'Artist'
+    } else {
+      let contractMethods = store.getters['blockSync/contractMethods']
+      let userAccount = store.state.blockSync.web3.coinbase
+      console.log(userAccount)
+
+
+      try {
+        console.log('wow1')
+        let name = await contractMethods.getListenerByAcc(userAccount).call()
+      } catch (err) {
+        console.log('wow2')
+        console.log(err)
+      }
+
+      store.state.user.name = name
+      return 'Listener'
+    }
+  }
 }
