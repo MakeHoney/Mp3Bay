1481ffb43ac36d74035f3d1dc6e2d97032e09b48
makehoney <makehoney3@gmail.com>
2018-12-05 18:58:16 +0900

    Change getEventsFromBlock -> event obj

--- server/src/utils/index.js
+++ server/src/utils/index.js
@@ -1,11 +1,11 @@
 import { getContract } from './getContract'
-import { getEventsFromBlock } from './getEventsFromBlock'
+import { event } from './getEventsFromBlock'
 import Lib from '../lib'
 
 const lib = new Lib()
 
 export const utils = {
   getContract,
-  getEventsFromBlock,
+  event,
   lib
 }
