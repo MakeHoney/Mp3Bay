9ba722ae3eac7c84a3cb73b27c35dcedebbd1780
makehoney <makehoney3@gmail.com>
2018-12-06 09:22:54 +0900

    4 -> 2 indent

--- client/src/components/artist-profile/Modal.vue
+++ client/src/components/artist-profile/Modal.vue
@@ -12,7 +12,22 @@
         <b-modal ref="registerForm" hide-footer title="곡 등록">
             <div class="d-block text-center">
                 <h3>곡 정보</h3>
-                <input type="text" v-model="title">
+                <b-row class="my-1">
+                    <b-col sm="2"><label>Title:</label></b-col>
+                    <b-col sm="9">
+                        <b-form-input id="input-songTitle"
+                                      type="text"
+                                      v-model="title">
+                        </b-form-input>
+                    </b-col>
+                </b-row>
+
+                <b-row class="my-1">
+                    <b-col sm="2"><label>File:</label></b-col>
+                    <b-col sm="9">
+                        <input type="file" @change="onFileSelected">
+                    </b-col>
+                </b-row>
             </div>
             <b-btn class="mt-3" variant="outline-danger" block @click="hideRegisterForm">등록</b-btn>
         </b-modal>
@@ -27,25 +42,26 @@
 </template>
 
 <script>
-export default {
+  export default {
     data() {
-        return {
-            title: ''
-        }
+      return {
+        title: ''
+      }
     },
     methods: {
-        showRegisterForm () {
-            this.$refs.registerForm.show()
-        },
-        hideRegisterForm () {
-            this.$refs.registerForm.hide()
-        },
-        showSongList () {
-            this.$refs.songList.show()
-        },
-        hideSongList () {
-            this.$refs.songList.hide()
-        }
+      showRegisterForm () {
+        this.$refs.registerForm.show()
+      },
+      hideRegisterForm () {
+        this.$refs.registerForm.hide()
+      },
+      showSongList () {
+        this.$refs.songList.show()
+      },
+      hideSongList () {
+        this.$refs.songList.hide()
+      },
+
     }
-}
+  }
 </script>
