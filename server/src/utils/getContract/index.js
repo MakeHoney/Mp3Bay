f0c9b468fbee2c134079aa2f00a4ac15215b7ff9
makehoney <makehoney3@gmail.com>
2018-12-08 09:54:44 +0900

    Change host

--- server/src/utils/getContract/index.js
+++ server/src/utils/getContract/index.js
@@ -1,8 +1,9 @@
 import Web3 from 'web3'
+import { config } from '../../config'
 import { ABI, address } from './contracts/manager'
 
-export const getContract = new Promise((resolve, reject) => {
-    let web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+export const getContract = new Promise(resolve => {
+    let web3 = new Web3(new Web3.providers.HttpProvider(config.LOCAL_PROVIDER))
     let contractInstance = new web3.eth.Contract(ABI, address)
     resolve(contractInstance)
 })
