1d054d68e7fb6510a77bafa2e4cc5676a1da4843
makehoney <makehoney3@gmail.com>
2018-12-08 20:04:20 +0900

    Switch the source(root) of contract

--- server/src/utils/getContract/contracts/manager.js
+++ server/src/utils/getContract/contracts/manager.js
@@ -1,13 +1,12 @@
 import truffleJSON from '../../../../../truffle/build/contracts/SongManager'
 
 const networks = truffleJSON.networks
-// const keys = Object.keys(networks)
-// const lastIdxOfKeys = keys.length - 1
+const keys = Object.keys(networks)
+const lastIdxOfKeys = keys.length - 1
 
 const ABI = truffleJSON.abi
-const address = networks['4649'].address
-// const address = networks[keys[lastIdxOfKeys]].address
-// const address = '0x1a782cc3fb11a481bf6e0f35437af3a049c6ac62'
+// const address = networks['4649'].address
+const address = networks[keys[lastIdxOfKeys]].address
 // console.log(ABI)
 console.log(address)
 
