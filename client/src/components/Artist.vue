cf1c32f3fd34ca20450275421db5d6cc16c02355
makehoney <makehoney3@gmail.com>
2018-12-08 15:30:27 +0900

    Implement Artist.vue

--- client/src/components/Artist.vue
+++ client/src/components/Artist.vue
@@ -1,27 +1,41 @@
 <template>
-    <b-card title="Zico"
-            img-src="http://localhost:8888/music/load?id=0"
+    <b-card :title="name"
+            :img-src="pictureHost"
             img-alt="Image"
             img-top
             tag="article"
-            style="max-width: 20rem;"
             class="mb-2">
         <p class="card-text">
             Zico는 래퍼이다.
         </p>
-        <b-button href="#" variant="primary">Go somewhere</b-button>
+        <b-button href="#" variant="danger">Go somewhere</b-button>
     </b-card>
 </template>
 
 <script>
+  import { mapState } from 'vuex'
   export default {
     data() {
       return {
-        id: this.$route.params.id,
-        name: this.$route.params.name
+
       }
     },
-
+    computed: {
+      ...mapState([
+        'artists',
+        'apiHost'
+      ]),
+      pictureHost() {
+        return `${this.apiHost}/artist/load-picture?id=${this.artistID}`
+      }
+    },
+    props: {
+      artistID: {
+        type: String
+      },
+      name: {
+        type: String
+      }
+    }
   }
 </script>
-
