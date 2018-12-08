eb400208c6ca72e2b9d331b8f9ed185fb0ee3b97
makehoney <makehoney3@gmail.com>
2018-12-08 13:47:58 +0900

    4 -> 2indent

--- client/src/components/Artist.vue
+++ client/src/components/Artist.vue
@@ -1,19 +1,27 @@
 <template>
-    <div class="artist">
-        <div>
-            <li>아티스트 명: {{ name }}</li>
-        </div>
-    </div>
+    <b-card title="Zico"
+            img-src="http://localhost:8888/music/load?id=0"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+            class="mb-2">
+        <p class="card-text">
+            Zico는 래퍼이다.
+        </p>
+        <b-button href="#" variant="primary">Go somewhere</b-button>
+    </b-card>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                id: this.$route.params.id,
-                name: this.$route.params.name
-            }
-        }
-    }
+  export default {
+    data() {
+      return {
+        id: this.$route.params.id,
+        name: this.$route.params.name
+      }
+    },
+
+  }
 </script>
 
