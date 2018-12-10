c8c2420cc9b4ba1f358acacd9be2fb9dbf6e1077
makehoney <makehoney3@gmail.com>
2018-12-10 10:46:44 +0900

    Add transition and font

--- client/src/components/Artist.vue
+++ client/src/components/Artist.vue
@@ -1,16 +1,19 @@
 <template>
-    <b-card :title="name"
-            :img-src="pictureHost"
-            img-alt="Image"
-            img-top
-            tag="article"
-            class="mb-2">
-        <p class="card-text">
-            <!-- 길어지면 ... 표시 -->
-            {{description}}
-        </p>
-        <b-button variant="danger" @click="triggerArtistDetail">Detail</b-button>
-    </b-card>
+    <transition name="router-anim" enter-active-class="animated fadeInDown" leave-active-class="animated fadeOutDown">
+        <b-card :title="name"
+                :img-src="pictureHost"
+                img-alt="Image"
+                img-top
+                tag="article"
+                class="mb-2"
+                id="card-section">
+            <p id="card-text">
+                <!-- TODO: 길어지면 ... 표시 -->
+                {{description}}
+            </p>
+            <b-button variant="danger" @click="triggerArtistDetail">Detail</b-button>
+        </b-card>
+    </transition>
 </template>
 
 <script>
@@ -27,9 +30,6 @@
         'artists',
         'apiHost'
       ])
-      // pictureHost() {
-      //   return `${this.apiHost}/artist/load-picture?id=${this.artistID}`
-      // }
     },
     methods: {
       triggerArtistDetail () {
@@ -57,3 +57,15 @@
     }
   }
 </script>
+<style scoped>
+    @import url("http://fonts.googleapis.com/css?family=Oleo+Script");
+    @import url("http://fonts.googleapis.com/earlyaccess/nanummyeongjo.css");
+    #card-section{
+        font: 50px/1.2 'Oleo Script', Helvetica, sans-serif;
+    }
+    #card-text {
+        font-family: 'Nanum Myeongjo';
+        font-size: 18px;
+        font-weight: bold;
+    }
+</style>
