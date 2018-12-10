b940fc1abda42e1832f8eacf9e0c918424839e9b
makehoney <makehoney3@gmail.com>
2018-12-10 10:47:46 +0900

    Add style for transition

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,9 @@
       <nav-bar/>
       <div class="container">
           <keep-alive include="player">
-              <router-view></router-view>
+              <transition name="router-anim">
+                  <router-view></router-view>
+              </transition>
           </keep-alive>
       </div>
   </div>
@@ -32,79 +34,108 @@
 
 
 <style>
-/* Make clicks pass-through */
-#nprogress {
-  pointer-events: none;
-}
+    @import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+    @import url("http://fonts.googleapis.com/css?family=Oleo+Script");
+    /* Make clicks pass-through */
+    #nprogress {
+        pointer-events: none;
+    }
 
-#nprogress .bar {
-  background: white;
+    #nprogress .bar {
+        background: white;
 
-  position: fixed;
-  z-index: 1031;
-  top: 0;
-  left: 0;
+        position: fixed;
+        z-index: 1031;
+        top: 0;
+        left: 0;
 
-  width: 100%;
-  height: 2px;
-}
+        width: 100%;
+        height: 2px;
+    }
 
-/* Fancy blur effect */
-#nprogress .peg {
-  display: block;
-  position: absolute;
-  right: 0px;
-  width: 100px;
-  height: 100%;
-  box-shadow: 0 0 10px white, 0 0 5px white;
-  opacity: 1.0;
+    /* Fancy blur effect */
+    #nprogress .peg {
+        display: block;
+        position: absolute;
+        right: 0px;
+        width: 100px;
+        height: 100%;
+        box-shadow: 0 0 10px white, 0 0 5px white;
+        opacity: 1.0;
 
-  -webkit-transform: rotate(3deg) translate(0px, -4px);
-      -ms-transform: rotate(3deg) translate(0px, -4px);
-          transform: rotate(3deg) translate(0px, -4px);
-}
+        -webkit-transform: rotate(3deg) translate(0px, -4px);
+        -ms-transform: rotate(3deg) translate(0px, -4px);
+        transform: rotate(3deg) translate(0px, -4px);
+    }
 
-/* Remove these to get rid of the spinner */
-#nprogress .spinner {
-  display: block;
-  position: fixed;
-  z-index: 1031;
-  top: 15px;
-  right: 15px;
-}
+    /* Remove these to get rid of the spinner */
+    #nprogress .spinner {
+        display: block;
+        position: fixed;
+        z-index: 1031;
+        top: 15px;
+        right: 15px;
+    }
 
-#nprogress .spinner-icon {
-  width: 18px;
-  height: 18px;
-  box-sizing: border-box;
+    #nprogress .spinner-icon {
+        width: 18px;
+        height: 18px;
+        box-sizing: border-box;
 
-  border: solid 2px transparent;
-  border-top-color: white;
-  border-left-color: white;
-  border-radius: 50%;
+        border: solid 2px transparent;
+        border-top-color: white;
+        border-left-color: white;
+        border-radius: 50%;
 
-  -webkit-animation: nprogress-spinner 400ms linear infinite;
-          animation: nprogress-spinner 400ms linear infinite;
-}
+        -webkit-animation: nprogress-spinner 400ms linear infinite;
+        animation: nprogress-spinner 400ms linear infinite;
+    }
 
-.nprogress-custom-parent {
-  overflow: hidden;
-  position: relative;
-}
+    .nprogress-custom-parent {
+        overflow: hidden;
+        position: relative;
+    }
 
-.nprogress-custom-parent #nprogress .spinner,
-.nprogress-custom-parent #nprogress .bar {
-  position: absolute;
-}
+    .nprogress-custom-parent #nprogress .spinner,
+    .nprogress-custom-parent #nprogress .bar {
+        position: absolute;
+    }
 
-@-webkit-keyframes nprogress-spinner {
-  0%   { -webkit-transform: rotate(0deg); }
-  100% { -webkit-transform: rotate(360deg); }
-}
-@keyframes nprogress-spinner {
-  0%   { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
-}
+    @-webkit-keyframes nprogress-spinner {
+        0%   { -webkit-transform: rotate(0deg); }
+        100% { -webkit-transform: rotate(360deg); }
+    }
+    @keyframes nprogress-spinner {
+        0%   { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
 
+    .router-anim-enter-active {
+        animation: coming 1s;
+        animation-delay: .2s;
+        opacity: 0;
+    }
+    .router-anim-leave-active {
+        animation: going .2s;
+    }
 
+    @keyframes going {
+        from {
+            transform: translateX(0);
+        }
+        to {
+            transform: translateX(-50px);
+            opacity: 0;
+        }
+    }
+    @keyframes coming {
+        from {
+            transform: translateX(-50px);
+            opacity: 0;
+        }
+        to {
+            transform: translateX(0);
+            opacity: 1;
+        }
+    }
 </style>
