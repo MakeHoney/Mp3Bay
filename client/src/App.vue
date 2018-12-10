c47c9587ab4d89ff38911e34844289c4398b3c95
makehoney <makehoney3@gmail.com>
2018-12-10 20:14:04 +0900

    4 -> 2indent

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,35 +1,37 @@
 <template>
-  <div id="app">
-      <nav-bar/>
-      <div class="container">
-          <keep-alive include="player">
-              <transition name="router-anim">
-                  <router-view></router-view>
-              </transition>
-          </keep-alive>
-      </div>
-  </div>
+    <div id="app">
+        <nav-bar/>
+        <div clas1s="container">
+            <!--<player/>-->
+            <keep-alive include="player">
+                <transition name="router-anim">
+                    <router-view></router-view>
+                </transition>
+            </keep-alive>
+        </div>
+    </div>
 </template>
 
 <script>
-    import NavBar from './components/common/NavBar'
-    import { mapState } from 'vuex'
-    export default {
-        name: 'app',
-        computed: {
-            ...mapState({
-                user: state => state.user,
-                contractInstance: state => state.blockSync.contractInstance
-            })
-        },
-        components: {
-            NavBar
-        },
-        async created () {
-            await this.$store.dispatch('blockSync/checkWeb3')
-            await this.$store.dispatch('blockSync/getContractInstance')
-        }
+  import NavBar from './components/common/NavBar'
+  import { mapState } from 'vuex'
+  export default {
+    name: 'app',
+    computed: {
+      ...mapState({
+        user: state => state.user,
+        contractInstance: state => state.blockSync.contractInstance
+      })
+    },
+    components: {
+      NavBar
+    },
+    async created () {
+      await this.$store.dispatch('blockSync/checkWeb3')
+      await this.$store.dispatch('blockSync/getContractInstance')
+
     }
+  }
 </script>
 
 
