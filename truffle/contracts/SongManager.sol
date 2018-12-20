c04d46f63b536fdfc9526f2107b43217b6bd576d
makehoney <makehoney3@gmail.com>
2018-12-21 08:19:32 +0900

    Add features for flee market

--- truffle/contracts/SongManager.sol
+++ truffle/contracts/SongManager.sol
@@ -16,7 +16,12 @@ contract SongManager is Manager {
 
     SongLib.Song[] public songs;
 
-    mapping (address => mapping(uint => bool)) public listenerAccountToSongs;
+    // Last uint represents state of songs
+    // 0: unowned song
+    // 1: owned song
+    // 2: song posted on flee market
+    mapping (address => mapping(uint => uint)) public listenerAccToSongs;
+
     mapping (address => uint) public listenerAccountToSongCount;
     mapping (uint => address) public songIDToArtistAccount;
     mapping (address => uint) public artistAccountToSongCount;
@@ -54,7 +59,7 @@ contract SongManager is Manager {
         // pay for music
 
         // wrong condition sentence (id == 100) need to be changed
-        // require(listenerAccountToSongs[msg.sender].songIDToSong[_id].id == 100, "the song already exsits!");
+        // require(listenerAccToSongs[msg.sender].songIDToSong[_id].id == 100, "the song already exsits!");
         // require(msg.value == 1 ether, "not enough or too much ether to buy a song!");
         address artistAccount = songIDToArtistAccount[_id];
         artistAccount.transfer(msg.value);
@@ -63,8 +68,31 @@ contract SongManager is Manager {
         tokenAddr.transferFrom(msg.sender, artistAccount, 100);
 
         // add song into listener' song list.
-        listenerAccountToSongs[msg.sender][_id] = true;
+        listenerAccToSongs[msg.sender][_id] = 1;
         listenerAccountToSongCount[msg.sender]++;
+    }
+
+    function buySongFromFM (address seller, uint songID) {
+        require(listenerAccToSongs[seller][songID] == 2);
+        require(listenerAccToSongs[msg.sender][songID] == 0);
+        address artist = songIDToArtistAccount[songID];
+
+        tokenAddr.transferFrom(msg.sender, seller, 20);
+        tokenAddr.transferFrom(msg.sender, artist, 20);
+        tokenAddr.transferFrom(msg.sender, owner, 10);
+
+        listenerAccToSongs[seller][songID] = 0;
+        listenerAccToSongs[msg.sender][songID] = 1;
+    }
+
+    function postingSongOnFM(uint songID) public onlyListener(msg.sender) {
+        require(listenerAccToSongs[msg.sender][songID] == 1);
+        listenerAccToSongs[msg.sender][songID] = 2;
+    }
+
+    function removePosting(uint songID) public onlyListener(msg.sender) {
+        require(listenerAccToSongs[msg.sender][songID] == 2);
+        listenerAccToSongs[msg.sender][songID] = 1;
     }
 
     // is this needed?
@@ -83,12 +111,16 @@ contract SongManager is Manager {
         return songIDs;
     }
 
-    function getSongIDsByListenerAcc() public view returns (uint[]) {
+    function getSongIDsByListenerAcc(string str) public view returns (uint[]) {
         uint[] memory songIDs = new uint[](listenerAccountToSongCount[msg.sender]);
+        uint memory flag;
         uint count = 0;
+        if (str == "owned") flag = 1;
+        else if (str == "posted") flag = 2;
+        else flag = 3;
 
         for(uint i = 0; i < songs.length; i++) {
-            if(listenerAccountToSongs[msg.sender][i]) {
+            if(listenerAccToSongs[msg.sender][i] == flag) {
                 songIDs[count++] = i;
             }
         }
