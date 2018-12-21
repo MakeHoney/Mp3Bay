393e08986fef706f1f9bb5b2db648264ff065eb5
makehoney <makehoney3@gmail.com>
2018-12-21 14:16:57 +0900

    Add new components

---
+++ client/src/components/flee-market/FleeMarket.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h1 class="page-title">Flee Market</h1>
+        <hr class="page-border">
+        <button @click="foo">Post</button>
+
+        <div>
+            <post></post>
+            <post></post>
+            <post></post>
+        </div>
+    </div>
+</template>
+
+<script>
+  import Post from './Post'
+  import { blockEvent } from '../../utils'
+  export default {
+    name: 'flee-market',
+    components: {
+      Post
+    },
+    methods: {
+      async foo() {
+        // const events = await blockEvent.getEventsFromBlock('ListenerCreated')
+        // const result = await blockEvent.getDataFromEvents('ListenerCreated', events)
+        // console.log(result)
+        await this.$store.dispatch('getPosts')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    .page-title {
+        font: 50px/1.2 'Oleo Script', Helvetica, sans-serif;
+        text-align: center;
+        margin-top: 20px;
+    }
+    .page-border {
+        border-color: red;
+    }
+</style>
---
+++ client/src/components/flee-market/Post.vue
@@ -0,0 +1,14 @@
+<template>
+    <b-card border-variant="danger"
+            header="Danger"
+            header-border-variant="danger"
+            header-text-variant="danger"
+            class="mb-3"
+            align="center">
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </b-card>
+</template>
+
+<script>
+
+</script>
